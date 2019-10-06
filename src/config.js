87154bcfad6d66a49c7b3241c069f40890cfb953
Harry Chung <hc9825@gmail.com>
2019-10-05 17:30:19 -0700

    heroku fix

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: 'postgresql://postgres@localhost/goala' || process.env.DATABASE_URL,
-  // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/goala',
+  // DATABASE_URL: 'postgresql://postgres@localhost/goala' || process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/goala',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/goala-test',
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '60s',
