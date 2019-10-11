20b03c34847247d430334f9e7b5c1c4c51fb9455
Harry Chung <hc9825@gmail.com>
2019-10-10 23:47:58 -0700

    procfile

--- test/goals-endpoints.spec.js
+++ test/goals-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('Goals Endpoints', function() {
+describe('Goals Endpoints', function() {
     let db
 
     const {
--- test/reminders-endpoints.spec.js
+++ test/reminders-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('Reminders Endpoints', function() {
+describe('Reminders Endpoints', function() {
     let db
 
     const {
